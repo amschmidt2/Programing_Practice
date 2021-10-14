3ce645249a8b6322289f0a6ebeccf3994e11b591
amschmidt2 <38161639+amschmidt2@users.noreply.github.com>
2021-10-14 17:15:36 -0500

    Creates a new DriveTrain.

--- Basic_Auto/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ Basic_Auto/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,11 +4,37 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class DriveTrain extends SubsystemBase {
+  Spark leftFront;
+  Spark rightFront;
+  Spark leftBack;
+  Spark rightBack;
+  SpeedControllerGroup leftMotors;
+  SpeedControllerGroup rightMotors;
+  DifferentialDrive drive; 
+
   /** Creates a new DriveTrain. */
-  public DriveTrain() {}
+  public DriveTrain() {
+    leftFront = new Spark(Constants.LEFT_FRONT);
+    leftFront.setInverted(false);
+    rightFront = new Spark(Constants.RIGHT_FRONT);
+    rightFront.setInverted(false);
+    leftBack = new Spark(Constants.LEFT_BACK);
+    leftBack.setInverted(false);
+    rightBack = new Spark(Constants.RIGHT_BACK);
+    rightBack.setInverted(false);
+
+    leftMotors = new SpeedControllerGroup(leftFront, leftBack);
+    rightMotors = new SpeedControllerGroup(rightFront, rightBack);
+    drive = new DifferentialDrive(leftMotors, rightMotors);
+
+  }
 
   @Override
   public void periodic() {
