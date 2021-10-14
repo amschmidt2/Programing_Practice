127e255409d9a20b665e8f7de06764dff393bdb7
amschmidt2 <38161639+amschmidt2@users.noreply.github.com>
2021-10-14 17:15:26 -0500

    Update DriveTrain.java

--- Basic_Auto/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ Basic_Auto/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -29,10 +30,30 @@ DifferentialDrive drive;
     leftBack.setInverted(false);
     rightBack = new Spark(Constants.RIGHT_BACK);
     rightBack.setInverted(false);
+
+
+    leftMotors = new SpeedControllerGroup(leftFront, leftBack);
+    rightMotors = new SpeedControllerGroup(rightFront, rightBack);
+    drive = new DifferentialDrive(leftMotors, rightMotors);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+  
+  public void driveWitheJoysticks(XboxController controller, double speed)
+  {
+    drive.arcadeDrive(controller.getRawAxis(Constants.XBOX_LEFT_Y_AXIS)*speed, controller.getRawAxis(Constants.XBOX_LEFT_X_AXIS)*speed);
+  }
+
+  public void driveFoward(double speed)
+  {
+    drive.tankDrive(speed, speed);
+  }
+
+  public void stop()
+  {
+    drive.stopMotor();
   }
 }
