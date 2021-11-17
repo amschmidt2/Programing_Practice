d3b37fabde160493880076144d7bf3b78475e294
amschmidt2 <amschmidt2@cps.edu>
2021-11-17 17:15:42 -0600

    Video 3 3/4 done
    Almost done  with  vid 3

--- Borb Bot 2.0/src/main/java/frc/robot/RobotContainer.java
+++ Borb Bot 2.0/src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.DriveForwardTimed;
+import frc.robot.commands.DriveWithJoysticks;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -22,8 +25,26 @@ public class RobotContainer {
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
+  //Drivetrain declare
+  private final DriveTrain  driveTrain;
+  private final DriveWithJoysticks driveWithJoysticks;
+  private final DriveForwardTimed driveForwardTimed;
+  public static XboxController driverJoystick;
+
+
+  
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    driveTrain = new DriveTrain();
+    driveWithJoystick = new DriveWithJoysticks(driveTrain);
+    driveWithJoystick.addRequirements(driveTrain);
+    driveTrain.setDeefaultCommand(driveWithJoystick);
+
+    driveForwardTimed = new DriveForwardTimed(driveTrain);
+    driveForwardTimed.addRequirements(driveTrain);
+
+    driverJoystick = new XboxController(Constants.JOYSTICK_NUMBER);
+
     // Configure the button bindings
     configureButtonBindings();
   }
